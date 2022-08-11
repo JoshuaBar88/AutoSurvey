267f3f62bcb4a30a92d073d84bb5a940b4ea764e
Joshua Barnett <jobarnett@muhlenberg.edu>
2022-08-11 19:36:50 -0400

    Updated code for optimal

--- survey.py
+++ survey.py
@@ -1,3 +1,4 @@
+from logging.config import valid_ident
 from operator import truediv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -30,16 +31,16 @@ def getBasics(tag):
             'age': ['22'],
             'old': ['22'],
             'zip': ['06511'],
-            'state': [' ct ', 'Connecticut'],
+            'state': ['ct', 'connecticut'],
             'status': ['single'],
             'income': ['80000'],
             'household': ['80000'],
             'birth': [('date', '1'), ('day', '1'), ('year', '1998'), ('month', '9', 'september')],
             'gender': ['male', ' m'],
             'education': ['bachelor', 'b.a'],
-            'school': ['bachelor', 'b.a'],
-            'degree': ['bachelor', 'b.a'],
-            'employment': ['full time'],
+            'school': ['bachelor', 'ba', 'collegedegree', 'bachelorsdegree'],
+            'degree': ['bachelor', 'ba', 'collegedegree', 'bachelorsdegree'],
+            'employment': ['fulltime'],
             'race': ['black'],
             'ethnicity': ['black'],
             'hispanic': ['no'],
@@ -48,9 +49,10 @@ def getBasics(tag):
             'model': ['impala'],
             'male': ['male', 'm'],
             'female': ['male', 'm'],
-            'country': ['united states', 'usa', 'u.s.a', ' us ', 'u.s'],
-            'county': ['new haven'],
-            'city': ['new haven']
+            'country': ['unitedstates', 'usa', 'u.s.a', ' us ', 'u.s'],
+            'county': ['newhaven'],
+            'city': ['newhaven'],
+            'language': ['english']
     }
     for i,j in life_dict.items():
         for x in tag:
@@ -58,30 +60,55 @@ def getBasics(tag):
                 answers += j
     return set(answers)
 def answerOfBasics(element, types):
+    keys = {x:y for (x,y) in element.attrs.items() if x == 'id' or x == 'name'}
+    parent = question_container.find(element.name, attrs=keys)
+    childOfParent = parent.findChildren()
     if type(list(types)[0]) == str:
-        for i in element:
-            for j in types:
-                try:
-                    driver.find_element_by_xpath(f'//{i.name}[contains(translate(normalize-space(.), \'ABCDEFGHIJKLMNOPQRSTUVWXYZ\', \'abcdefghijklmnopqrstuvwxyz\'), \'{j}\')]').click()
-                    return True
-                except Exception:
-                    pass
-    else:
-        first = element[0]
         for j in types:
             try:
-                if j[0] in first.parent.text.lower(): 
-                    gottem = driver.find_element_by_xpath(f'//{first.parent.name}[contains(translate(normalize-space(.), \'ABCDEFGHIJKLMNOPQRSTUVWXYZ\', \'abcdefghijklmnopqrstuvwxyz\'),\'{j[0]}\')]')
+                listOfElements = textInChildren(parent, childOfParent, j)
+                if listOfElements:
+                    return True
+            except Exception:
+                pass
+    else:
+        for j in types:
+            try:
+                if j[0] in element.text.lower(): 
                     for x in range(1,len(j)):
                         try:
-                            if j[x] in gottem.text.lower():
-                                gottem.find_element_by_xpath(f'//{first.name}[contains(translate(normalize-space(.), \'ABCDEFGHIJKLMNOPQRSTUVWXYZ\', \'abcdefghijklmnopqrstuvwxyz\'),\'{j[x]}\')]').click()
-                                return True
+                            if j[x] in element.text.lower():
+                                listOfElements = textInChildren(parent, childOfParent, j[x])
+                                if listOfElements:
+                                    return True
                         except Exception:
                             pass
             except Exception:
                 pass
     return None
+def clickAll(parent, elements):
+    validList = ['div', 'option', 'input', 'button']
+    sorted = [x for x in elements if x.name in validList]
+    good = False
+    for i in sorted:
+        try:
+            if 'value' in i.attrs.keys() and i.name == 'option':
+                good = tryClickingSelect(parent, i)
+            else:
+                good = tryClicking(i)
+        except Exception:
+            pass
+    return good
+def textInChildren(parent, elements, key):
+    validList = ['div', 'option', 'input', 'button']
+    sorted = [x for x in elements if x.name in validList]
+    good = False
+    goodBoys = []
+    for i in sorted:
+        go = re.sub('[^A-Za-z0-9]+', '', i.getText().lower())
+        if go == key:
+            goodBoys.append(i)
+    return clickAll(parent, goodBoys)
 
 def getQuestionType(quest, type):
     #call APi if need be
@@ -324,14 +351,14 @@ def nextPage(html):
 def tryClicking(object):
     keys = object.attrs
     try:
-        if len(question_container.find_all(element.name, attrs={'id' : str(keys['id'])})) == 1:
+        if len(question_container.find_all(object.name, attrs={'id' : str(keys['id'])})) == 1:
             element = driver.find_element_by_id(keys['id'])
             driver.execute_script("arguments[0].click();", element)
             return True
     except:
         pass
     try:
-        if len(question_container.find_all(element.name, attrs={'name' : str(keys['name'])})) == 1:
+        if len(question_container.find_all(object.name, attrs={'name' : str(keys['name'])})) == 1:
             element = driver.find_element_by_name(keys['name'])
             driver.execute_script("arguments[0].click();", element) 
             return True
@@ -345,35 +372,7 @@ def tryClicking(object):
         return True
     except:
         return False
-def randoClick(element) :
-    attributes = element.attrs
-    keys = attributes.keys()
-    try:
-        for i in keys:
-            if i == 'id' and len(question_container.find_all(element.name, attrs={'id' : str(attributes['id'])})) == 1:
-                try:
-                    driver.find_element_by_xpath(f'//{element.name}[@id='+'\''+ attributes['id']+'\''+']').click()
-                    return True
-                except Exception:
-                    pass
-            elif i == 'name' and len(question_container.find_all(element.name, attrs={'name' : str(attributes['name'])})) == 1:
-                try:
-                    driver.find_element_by_xpath(f'//{element.name}[@name='+'\''+ attributes['name']+'\''+']').click()
-                    return True
-                except Exception:
-                    pass
-            elif i == 'class':
-                try:
-                    cssFinder =  ' '.join(attributes['class'])
-                    driver.find_element_by_xpath(f'//{element.name}[@class=\'{cssFinder}\']').click()
-                    return True
-                except Exception:
-                    pass
-    except Exception:
-        return False
          
-    # if randoClick(answer, correctKey) != False:
-    # driver.find_element_by_xpath(randoClick(answer, correctKey)).click()
 def compare(one, two):
     score = 0
     which = []
@@ -412,17 +411,15 @@ def noDisplay(element):
         return True
 def getNumberOfQuestion(elementList, questions):
     finale = {}
-    finalQuestions = []
     for i,j in elementList.items():
         finale[i] = {}
         finale[i]['questions'] = []
-        for x in j:
-            holder = x
-            while holder not in questions:
-                holder = holder.parent
-            if holder not in finale[i]['questions']:
-                finale[i]['questions'].append(holder)
-                finalQuestions.append(holder)
+        if len(j) >= 1:
+            for x in questions:
+                holder = x.findChildren()
+                sameName = [p for p in holder if p.name == j[0].name]
+                if len(sameName) >= 1:
+                    finale[i]['questions'].append(x)
     return finale
 def tryClickingSelect(element, answer):
     try:
@@ -433,8 +430,8 @@ def tryClickingSelect(element, answer):
     except:
         pass
     try:
-        if len(question_container.find_all(element.name, attrs={'id' : str(element.attrs['id'])})) == 1:
-            value = Select(driver.find_element_by_name(element.attrs['id']))
+        if len(question_container.find_all(element.id, attrs={'id' : str(element.attrs['id'])})) == 1:
+            value = Select(driver.find_element_by_id(element.attrs['id']))
             value.select_by_value(answer.attrs['value'])
             return True
     except:
@@ -445,8 +442,8 @@ def answer():
     global question_container
 
     options = Options()
-    url = 'https://surveymyopinion.researchnow.com/survey/standalone?id=05fa40c1-3f5f-453e-a6de-13fea7720b20'
-    #url = 'https://dkr1.ssisurveys.com/projects/estart?ekey=RwqiqMmC2faoI7gqezAr5w**&GID=6000446&sname=0y-fxX4umTymepORtzNfTXz2kQY'
+    #url = 'https://surveymyopinion.researchnow.com/survey/standalone?id=05fa40c1-3f5f-453e-a6de-13fea7720b20'
+    url = 'https://dkr1.ssisurveys.com/projects/estart?ekey=RwqiqMmC2faoI7gqezAr5w**&GID=6000446&sname=0y-fxX4umTymepORtzNfTXz2kQY'
     # url = 'https://survey.alchemer.com/s3/6953135/HRB-CT-Survey-2022-Restructures?respondent=6514c15a-9755-a189-2d3a-f53f3ac84ebc'
     #url = 'https://edgesurvey.innovatemr.net/#/survey/age?survNum=vzdQ7N36&supCode=654&PID=2107-ALd-umrf9-xioe3-pasfuf_-202201&Lang=english&langCode=EN&jb_id=8436148&chkOeValid=1&oeQuestionIds=8878&AGE=ageval&GENDER=genderval&REGION=regionval&cntryCode=US&supId=QLB&RAIJuris=false&uid=rEB2G4G4k7ijJrEJnyD4uoeDZRJK1GTqpEWnKor&isp=1&start=1&isQuestionsExists=1'
     #options.add_experimental_option("debuggerAddress","lcalhost:9222")
@@ -492,12 +489,12 @@ def answer():
             mainAttract = None
             completedQuestions = []
             answersToQuestions = None
-            # if soup_object.find('section') != None:
-            #     question_container = soup_object.find('section')
-            # if soup_object.find('form') != None:
-            #     question_container = soup_object.find('form')
-            # else:
-            question_container = soup_object
+            if soup_object.find('section') != None:
+                question_container = soup_object.find('section')
+            if soup_object.find('form') != None:
+                question_container = soup_object.find('form')
+            else:
+                question_container = soup_object
             listElements = [x for x in question_container.find_all('ul') if 'hidden' not in x.attrs.keys() and 'hidden' not in x.attrs.values()]
             selection = [x for x in question_container.find_all('select') if 'hidden' not in x.attrs.keys() and 'hidden' not in x.attrs.values()]
             tables = [x for x in question_container.find_all('tr') if 'hidden' not in x.attrs.keys() and 'hidden' not in x.attrs.values()]
@@ -509,7 +506,7 @@ def answer():
             questionNames = question_container.find_all('div', attrs={'name' : re.compile(r'question|Question')}) + question_container.find_all('span', attrs={'name' : re.compile(r'question|Question')})
             headerTag = question_container.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
             questionArray = set(questionClasses + questionIds + questionNames + headerTag)
-            if len(questionArray) <= 1:
+            if len(questionArray) == 0:
                 questionArray = question_container.find_all('div')
 
             mainAttract = getNumberOfQuestion(allOptions, questionArray)
@@ -517,6 +514,7 @@ def answer():
             for key,quest in mainAttract.items():
                 if key == 'selection':
                     answersToQuestions = getBasics(quest['questions'])
+                    workingKey = None
                     inputs_update = []
                     for i in range(len(selection)):
                         currentPageSource = driver.page_source
@@ -529,19 +527,26 @@ def answer():
                                 if len(answersToQuestions) == 0: 
                                     answer = getOptions(listOfOptions, len(listOfOptions))
                                     tryClickingSelect(selection[i], answer)
+                                    if workingKey != False:
+                                        workingKey = True
                                 else:
-                                    answer = answerOfBasics(listOfOptions, answersToQuestions)
+                                    answer = answerOfBasics(selection[i], answersToQuestions)
+                                    if workingKey != False:
+                                        workingKey = True
                                     if answer == None:
                                         answer = getOptions(listOfOptions, len(listOfOptions))
-                                        randoClick(answer)
+                                        clickAll(selection[i], [answer])
+                                        if workingKey != False:
+                                            workingKey = True
                         except Exception:
                             pass
-                completedQuestions += quest['questions']
-                if key == 'listElements':
+                    if workingKey:
+                        completedQuestions += quest['questions']
+                if key == 'listElements' and len([x for x in completedQuestions if x in quest['questions']]) == 0:
                     answersToQuestions = getBasics(quest['questions'])
+                    workingKey = None
                     inputs_update = []
                     for i in range(len(listElements)):
-                        numberofq -= 1
                         liInputs = [x.find('input') for x in listElements[i].find_all('li') if x.find('input') != None]
                         if len(liInputs) == 0:
                             liInputs = [x.find('a') for x in listElements[i].find_all('li') if x.find('a') != None]
@@ -551,19 +556,27 @@ def answer():
                             if mult == 'mult':
                                 for i in range(int(len(liInputs)/2)):
                                     answer = getOptions(liInputs, len(liInputs))
-                                    randoClick(answer)
+                                    tryClicking(answer)
+                                    if workingKey != False:
+                                        workingKey = True
                             else:
                                 if len(answersToQuestions) == 0: 
                                     answer = getOptions(liInputs, len(liInputs))
+                                    tryClicking(answer)
+                                    if workingKey != False:
+                                        workingKey = True
                                 else:
                                     answer = answerOfBasics(liInputs, answersToQuestions)
                                     if answer == None:
                                         answer = getOptions(listOfOptions, len(listOfOptions))
-                                        randoClick(answer)
+                                        tryClicking(answer)
+                                        if workingKey != False:
+                                            workingKey = True
                         except Exception:
                             pass
-                if key == 'tables':
+                if key == 'tables' and len([x for x in completedQuestions if x in quest['questions']]) == 0:
                     answersToQuestions = getBasics(quest['questions'])
+                    workingKey = None
                     inputs_update = []
                     for i in range(len(tables)):
                         try:
@@ -598,20 +611,30 @@ def answer():
                                 if mult == 'mult':
                                     for i in range(int(len(inputs_update)/2)):
                                         answer = getOptions(inputs_update, len(inputs_update))
-                                        if randoClick(answer, correctKey) == False:
+                                        if tryClicking(answer, correctKey) == False:
                                             tryClicking(answer)
+                                        if workingKey != False:
+                                            workingKey = True
                                 else:
                                     if len(answersToQuestions) == 0: 
                                         answer = getOptions(inputs_update, len(inputs_update))
+                                        tryClicking(answer)
+                                        if workingKey != False:
+                                            workingKey = True
                                     else:
                                         answer = answerOfBasics(inputs_update, answersToQuestions)
                                         if answer == None:
                                             answer = getOptions(listOfOptions, len(listOfOptions))
-                                        randoClick(answer)
+                                            tryClicking(answer)
+                                            if workingKey != False:
+                                                workingKey = True
                         except Exception:
                             pass
-                if key == 'only_inputs':
+                if key == 'only_inputs' and len([x for x in completedQuestions if x in quest['questions']]) == 0:
                     answersToQuestions = getBasics(quest['questions'])
+                    if len(answersToQuestions) == 0:
+                        answersToQuestions = getBasics(questionArray)
+                    workingKey = None
                     inputs_update = []
                     try:
                         for i in range(len(only_inputs)):
@@ -623,15 +646,22 @@ def answer():
                             if mult == 'mult':
                                 for i in range(int(len(inputs_update)/2)):
                                     answer = getOptions(inputs_update, len(inputs_update))
-                                    randoClick(answer)
+                                    tryClicking(answer)
+                                    if workingKey != False:
+                                        workingKey = True
                             else:
                                 if len(answersToQuestions) == 0: 
                                     answer = getOptions(inputs_update, len(inputs_update))
+                                    tryClicking(answer)
+                                    if workingKey != False:
+                                        workingKey = True
                                 else:
                                     answer = answerOfBasics(inputs_update, answersToQuestions)
                                     if answer == None:
                                         answer = getOptions(inputs_update, len(inputs_update))
-                                        randoClick(answer)
+                                        tryClicking(answer)
+                                        if workingKey != False:
+                                            workingKey = True
                     except Exception:
                         pass
             
@@ -641,7 +671,7 @@ def answer():
                 if noDisplay(next_page[i]):
                     next_page = checkWithPage(currentPageSource, next_page[i], next_page[i].name)
                     break
-            randoClick(next_page)
+            tryClicking(next_page)
         except Exception as e:
             print(e)
             #go = False
